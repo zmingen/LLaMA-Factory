d99adc644b0d215395c2d4fbd8658695f7d6436b
zmingen <zmingen@gmail.com>
2024-12-06 12:23:14 +0800

    data augmentation

--- convert2submit.sh
+++ convert2submit.sh
@@ -1,2 +1,1 @@
-python convert2submit.py --test_file="data/mire/test1/test1.json" --prediction_file="saves/qwen2_
-vl-7b/lora/sft-infer_02/generated_predictions.jsonl" --save_path="submit_002.csv"
+python convert2submit.py --test_file="data/mire/test1/test1.json" --prediction_file="saves/qwen2_vl-7b/lora/sft-infer_04/generated_predictions.jsonl" --save_path="submit_004.csv"

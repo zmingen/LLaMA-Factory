905c3e0b6b8276bfcf7ed3b343a5733ebe4f57cc
zmingen <zmingen@gmail.com>
2024-12-10 17:18:44 +0800

    dora fine tune

--- convert2submit.sh
+++ convert2submit.sh
@@ -1,1 +1,1 @@
-python convert2submit.py --test_file="data/mire/test1/test1.json" --prediction_file="saves/qwen2_vl-7b/lora/sft-infer_04/generated_predictions.jsonl" --save_path="submit_004.csv"
+python convert2submit.py --test_file="data/mire/test1/test1.json" --prediction_file="saves/qwen2_vl-7b/lora/sft-infer_06/generated_predictions.jsonl" --save_path="submit_006.csv"
